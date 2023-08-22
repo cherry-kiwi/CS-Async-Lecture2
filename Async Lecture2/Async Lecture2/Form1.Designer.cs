8705f585a82b06639d9988ee7ea4f17decf0bfa0
cherry-kiwi <hmongsangga@gmail.com>
2023-08-22 15:17:54 +0900

    Done!

---
+++ Async Lecture2/Async Lecture2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Async_Lecture2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(79, 32);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(97, 12);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(79, 32);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Location = new System.Drawing.Point(191, 22);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(11, 12);
+            this.lblIndex.TabIndex = 2;
+            this.lblIndex.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblIndex;
+    }
+}
+
---
+++ Async Lecture2/Async Lecture2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async_Lecture2
+{
+    public partial class Form1 : Form
+    {
+        //비동기 취소: CancellationTokenSource
+        //비동기 취소 여부 추적: CancellationToken
+
+        CancellationTokenSource tokenSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ////비동기 취소
+        //private async Task DoWorkAsync(CancellationToken token)
+        //{
+        //    while (!token.IsCancellationRequested) //무한루프
+        //    {
+        //        await Task.Delay(100); //딜레이
+        //        int.TryParse(lblIndex.Text, out var value);
+        //        lblIndex.Text = (++value).ToString();
+        //    }
+        //}
+
+        //비동기 취소 추적
+        private async Task DoWorkAsync(CancellationToken token)
+        {
+            while (true) //무한루프
+            {
+                token.ThrowIfCancellationRequested(); //Cancel()이 호출이 되면 에러를 발생시킴
+                await Task.Delay(100); //딜레이
+                int.TryParse(lblIndex.Text, out var value);
+                lblIndex.Text = (++value).ToString();
+            }
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            tokenSource = new CancellationTokenSource(); //객체생성
+            var token = tokenSource.Token;
+
+            //비동기 취소 추적
+            try
+            {
+                await DoWorkAsync(token);
+            }
+            catch (OperationCanceledException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            ////비동기 취소
+            //await DoWorkAsync(token);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            tokenSource.Cancel();
+        }
+    }
+}
